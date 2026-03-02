23d7b639327c15af21f2d20c3917db231a47ce37
ArijSaaleh <arij.saleh.pro@gmail.com>
2026-03-02 17:02:29 +0100

    delete

--- frontend/src/pages/admin/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as AgencyManagement } from './AgencyManagement';
-export { default as Users } from './Users';
-export { default as SystemSettings } from './SystemSettings';
